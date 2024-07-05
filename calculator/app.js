68864b63dfa00aeb78eccad4b32df134448d735f
Benisha14 <balan1041968@gmail.com>
2024-07-05 11:20:34 +0530

    Add files via upload

---
+++ calculator/app.js
@@ -0,0 +1,19 @@
+// function display(value) {
+//   document.getElementById('result').value += value;
+// }
+//  function clearResult() {
+//   document.getElementById('result').value = '';
+//  }
+//   function calculateResult(){
+//     try{
+//    document.getElementById('result').value
+//    =
+//   eval(document.getElementById('result').
+//   value);
+//     }catch (e) {
+//       alert('Invalid input');
+//       clearResult();
+//     }
+//   } 
+
+
